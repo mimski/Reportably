f0a8de7656a3484c64b071beb373a067240d2756
mimski <maria.bg.1989@gmail.com>
2020-02-05 20:28:48 +0200

    Add Report model in service layer

---
+++ Reportably/Reportably.Services/Models/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reportably.Services.Models
+{
+    public class Report
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Summary { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        public string Author { get; set; }
+
+        //[Required]
+        //public string UserId { get; set; }
+
+        //public User User { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
