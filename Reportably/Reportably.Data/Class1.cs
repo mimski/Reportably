da6c333f5557e9aa6c5afc3b067645e55747e883
mimski <maria.bg.1989@gmail.com>
2020-02-05 20:27:58 +0200

    Add DbContext

--- Reportably/Reportably.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Reportably.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ Reportably/Reportably.Data/ReportablyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Reportably.Entities;
+
+namespace Reportably.Web.Data
+{
+    public class ReportablyDbContext : IdentityDbContext<User>
+    {
+        public ReportablyDbContext(DbContextOptions<ReportablyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ReportEntity> Reports { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.Entity<ReportEntity>()
+            //    .HasOne(user => user.User)
+            //    .WithMany(user => user.Reports)
+            //    .HasForeignKey(user => user.UserId);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
