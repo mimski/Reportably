7a7875e2638285acbcdbd536f828a79b0ad407d4
mimski <maria.bg.1989@gmail.com>
2020-02-05 21:22:00 +0200

    Add ReportViewModel

---
+++ Reportably/Reportably.Web/Models/ReportViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Reportably.Web.Models
+{
+    public class ReportViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Summary { get; set; }
+
+        [Required]
+        [Display(Name = "Publication Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublicationDate { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Author { get; set; }
+
+        //[Required]
+        //public string UserId { get; set; }
+
+        //public User User { get; set; }
+
+        [Display(Name = "Created On")]
+        [DataType(DataType.Date)]
+        public DateTime? ModifiedOn { get; set; }
+
+        [Display(Name = "Modified On")]
+        [DataType(DataType.Date)]
+        public DateTime? CreatedOn { get; set; }
+
+        [Display(Name = "Deleted On")]
+        [DataType(DataType.Date)]
+        public DateTime? DeletedOn { get; set; }
+
+        [Display(Name = "Deleted")]
+        [DataType(DataType.Date)]
+        public bool IsDeleted { get; set; }
+    }
+}
