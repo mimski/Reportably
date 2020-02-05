c8b5bad400f8ddc3cc21b8b1db29718415d25749
mimski <maria.bg.1989@gmail.com>
2020-02-05 20:30:17 +0200

    Add GlobalConstants

---
+++ Reportably/Reportably.Web/Utils/GlobalConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reportably.Web.Utils
+{
+    public class GlobalConstants
+    {
+        public const string AdministratorRole = "Administrator";
+        public const string AdministratorArea = "Admin";
+        public const string SuccessMessage = "SuccessMessage";
+    }
+}
