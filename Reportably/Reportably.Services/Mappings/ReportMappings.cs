3741cdaf0f5d45741f36058721310586aa308c4a
mimski <maria.bg.1989@gmail.com>
2020-02-05 20:29:15 +0200

    Add ReportMappings in service layer

---
+++ Reportably/Reportably.Services/Mappings/ReportMappings.cs
@@ -0,0 +1,51 @@
+﻿using Reportably.Entities;
+using Reportably.Services.Mappings.Extensions;
+using Reportably.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reportably.Services.Mappings
+{
+    internal static class ReportMappings
+    {
+        public static Report ToService(this ReportEntity entity)
+        {
+            return entity != null
+                ? new Report
+                {
+                    Id = entity.Id,
+                    Title = entity.Title,
+                    Summary = entity.Summary,
+                    Author = entity.Author,
+                    PublicationDate = entity.PublicationDate,
+                    CreatedOn = entity.CreatedOn,
+                    ModifiedOn = entity.ModifiedOn,
+                    DeletedOn = entity.DeletedOn,
+                    IsDeleted = entity.IsDeleted
+                }
+                : null;
+        }
+
+        public static ReportEntity ToEntity(this Report model)
+        {
+            return model != null
+                ? new ReportEntity
+                {
+                    Id = model.Id,
+                    Title = model.Title,
+                    Summary = model.Summary,
+                    Author = model.Summary,
+                    PublicationDate = model.PublicationDate,
+                    CreatedOn = model.CreatedOn,
+                    ModifiedOn = model.ModifiedOn,
+                    DeletedOn = model.DeletedOn,
+                    IsDeleted = model.IsDeleted
+                }
+                : null;
+        }
+
+        public static IReadOnlyCollection<Report> ToService(this IReadOnlyCollection<ReportEntity> entities)
+            => entities.MapCollection(ToService);
+    }
+}
