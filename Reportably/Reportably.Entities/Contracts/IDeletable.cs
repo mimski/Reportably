c1d51b9158d5efccbbc76e7cab788e043ab60076
mimski <maria.bg.1989@gmail.com>
2020-02-05 19:52:39 +0200

    Add IDeletable

---
+++ Reportably/Reportably.Entities/Contracts/IDeletable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Reportably.Entities.Contracts
+{
+    public interface IDeletable
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
