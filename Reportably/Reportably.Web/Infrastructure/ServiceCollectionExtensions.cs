dd7ec8209ec61390b304aea8b3eca191de909429
mimski <maria.bg.1989@gmail.com>
2020-02-05 20:12:22 +0200

    Add ServiceCollectionExtensions

---
+++ Reportably/Reportably.Web/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reportably.Services.Contracts;
+using Reportably.Services.Implementations;
+
+namespace Reportably.Web.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBusiness(this IServiceCollection services)
+        {
+            services.AddScoped<IReportService, ReportService>();
+
+            return services;
+        }
+    }
+}
