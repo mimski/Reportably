4fdea5706afae669a59b8e7a7bb099dc54f0f052
mimski <maria.bg.1989@gmail.com>
2020-02-05 19:52:51 +0200

    Add User entity

---
+++ Reportably/Reportably.Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Reportably.Entities
+{
+    public class User : IdentityUser
+    {
+        //public ICollection<ReportEntity> Reports { get; set; }
+    }
+}
