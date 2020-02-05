053f8061972da1434d42df9b982cce702f171155
mimski <maria.bg.1989@gmail.com>
2020-02-05 20:12:59 +0200

    Add ConfigurationExtensions

---
+++ Reportably/Reportably.Web/Infrastructure/ConfigurationExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Reportably.Web.Infrastructure
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetDefaultConnectionString(this IConfiguration configuration)
+            => configuration.GetConnectionString("DefaultConnection");
+    }
+}
