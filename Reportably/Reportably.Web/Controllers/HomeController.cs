92badb4f3f371a624feae1677b8c0db86b850abc
mimski <maria.bg.1989@gmail.com>
2020-02-05 20:13:39 +0200

    Update HomeController.cs

--- Reportably/Reportably.Web/Controllers/HomeController.cs
+++ Reportably/Reportably.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Reportably.Web.Models;
@@ -11,13 +7,6 @@ namespace Reportably.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
