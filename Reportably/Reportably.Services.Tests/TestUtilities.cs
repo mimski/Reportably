a0b34128b5376130c50daea3f898cd513507f744
mimski <maria.bg.1989@gmail.com>
2020-02-07 03:36:14 +0200

    Add TestUtilities in Service unit tests

---
+++ Reportably/Reportably.Services.Tests/TestUtilities.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reportably.Web.Data;
+
+namespace Reportably.Services.Tests
+{
+    public static class TestUtilities
+    {
+        public static DbContextOptions<ReportablyDbContext> GetOptions(string databaseName)
+        {
+            return new DbContextOptionsBuilder<ReportablyDbContext>()
+                .UseInMemoryDatabase(databaseName)
+                .Options;
+        }
+    }
+}
