65e3ff8b87ebf2a1fe1bf64fb4aef2c8f6d9d811
mimski <maria.bg.1989@gmail.com>
2020-02-07 03:36:53 +0200

    Add unit test for ReportService constructor

---
+++ Reportably/Reportably.Services.Tests/ReportServiceTests/Constructor_Should.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reportably.Services.Implementations;
+using Reportably.Web.Data;
+using System;
+
+namespace Reportably.Services.Tests.ReportServiceTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ThrowsArgumentNullException_WhenPassedContextIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new ReportService(null));
+        }
+
+        [TestMethod]
+        public void ReturnInstance()
+        {
+            var contextOptions = TestUtilities.GetOptions(nameof(ReturnInstance));
+
+            using (var context = new ReportablyDbContext(contextOptions))
+            {
+                var SUT = new ReportService(context);
+
+                Assert.IsNotNull(SUT);
+                Assert.IsInstanceOfType(SUT, typeof(ReportService));
+            }
+        }
+    }
+}
