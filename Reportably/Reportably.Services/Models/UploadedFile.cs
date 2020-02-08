7f0da40a2d4fa84bd1936e922303d1bd39c6348f
mimski <maria.bg.1989@gmail.com>
2020-02-08 19:22:27 +0200

    Add UploadFile model in Service layer

---
+++ Reportably/Reportably.Services/Models/UploadedFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Reportably.Services.Models
+{
+    public class UploadedFile
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long Lenght { get; set; }
+
+        public byte[] FileContent { get; set; }
+
+        public string ContentType { get; set; }
+
+        public Guid ReportId { get; set; }
+    }
+}
