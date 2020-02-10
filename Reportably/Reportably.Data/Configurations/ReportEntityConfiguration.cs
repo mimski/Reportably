8f1b77edf47e28dcb2c34a81c76b10e2ca800634
mimski <maria.bg.1989@gmail.com>
2020-02-10 14:20:44 +0200

    Add FluentAPI configuration for ReportEntity - set default value to 0 for DownloadCount property

---
+++ Reportably/Reportably.Data/Configurations/ReportEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Reportably.Entities;
+
+namespace Reportably.Data.Configurations
+{
+    internal class ReportEntityConfiguration : IEntityTypeConfiguration<ReportEntity>
+    {
+        public void Configure(EntityTypeBuilder<ReportEntity> builder)
+        {
+            builder.ToTable("Reports");
+
+            builder.HasKey(report => report.Id);
+
+            builder.Property(report => report.DownloadCount)
+               .HasDefaultValue(0);
+        }
+    }
+}
