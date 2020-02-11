72c1c633f1a41d0a7bb9e0458e8725eff15ba436
mimski <maria.bg.1989@gmail.com>
2020-02-11 02:35:38 +0200

    Add unit tests for GetTotalReportsbCountAsync

---
+++ Reportably/Reportably.Services.Tests/ReportServiceTests/GetTotalReportsbCountAsync_Should.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reportably.Entities;
+using Reportably.Services.Implementations;
+using Reportably.Services.Models;
+using Reportably.Web.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reportably.Services.Tests.ReportServiceTests
+{
+    [TestClass]
+    public class GetTotalReportsbCountAsync_Should
+    {
+        [TestMethod]
+        public async Task ReturnTotalReportsCount()
+        {
+            var options = TestUtilities.GetOptions(nameof(ReturnTotalReportsCount));
+
+            using (var actContext = new ReportablyDbContext(options))
+            {
+                actContext.Reports.Add(new ReportEntity());
+                actContext.Reports.Add(new ReportEntity());
+                actContext.Reports.Add(new ReportEntity());
+                actContext.Reports.Add(new ReportEntity());
+                actContext.Reports.Add(new ReportEntity());
+                actContext.Reports.Add(new ReportEntity());
+
+                await actContext.SaveChangesAsync();
+            }
+            using (var assertContext = new ReportablyDbContext(options))
+            {
+                var SUT = new ReportService(assertContext);
+                var result = await SUT.GetTotalReportsbCountAsync(new CancellationToken());
+
+                Assert.AreEqual(6, result.TotalReports);
+            }
+        }
+
+        [TestMethod]
+        public async Task ReturnIstanceOfReportSystem()
+        {
+            var options = TestUtilities.GetOptions(nameof(ReturnIstanceOfReportSystem));
+
+            using (var assertContext = new ReportablyDbContext(options))
+            {
+                var SUT = new ReportService(assertContext);
+                var reports = await SUT.GetTotalReportsbCountAsync(new CancellationToken());
+
+                Assert.IsInstanceOfType(reports, typeof(ReportSystem));
+            }
+        }
+    }
+}
